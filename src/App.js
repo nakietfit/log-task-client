f8540e3f3d380f31f8749392857c2b14a911f614
DESKTOP-6AE2BR5\ANHKIET <nakietfit@gmail.com>
2019-06-01 21:20:39 +0700

    Authentication

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import TaskList from './components/TaskList'
 import CreateTask from './components/CreateTask'
 import Header from './components/Header'
+import Login from './components/Login'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={TaskList} />
             <Route exact path="/create" component={CreateTask} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
+import { AUTH_TOKEN } from '../constants'
 
 class Header extends Component {
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN)
+
     return (
       <div>
         <div>
@@ -11,10 +14,30 @@ class Header extends Component {
           <Link to="/">
             new
           </Link>
-          <div>|</div>
-          <Link to="/create">
-            submit
-          </Link>
+          {authToken && (
+            <div>
+              <div>|</div>
+              <Link to="/create">
+                submit
+              </Link>
+            </div>
+          )}
+        </div>
+        <div>
+          {authToken ? (
+            <div
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN)
+                this.props.history.push(`/`)
+              }}
+            >
+              logout
+            </div>
+          ) : (
+            <Link to="/login">
+              login
+            </Link>
+          )}
         </div>
       </div>
     )
